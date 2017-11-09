f8c18fff1eea70404829a7a58906872a2f07e65f
Sino33 <louis.desserteaux@gmail.com>
2017-11-09 11:00:45 +0100

    rejout de page3, modif de index

---
+++ index - Copie.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+<!-- créer un formulaire d'inscription: champs : pseudo, email et mdp
+le champ mdp doit être répété 2 fois (mdp et mdp 2 par exemple)
+method post       action="inscription.php"
+sur inscription.php faire toutes les vérifs nécessaires: champs pseudo, email et mdp pas vides
+email valide. Le pseudo et le mdp doivent faire entre 4 et 10 caractères
+les deux champs mdp identiques 
+Si erreur(s) : affichage de messages
+Si tout est bon -> insertion dans la bdd
+
+
+Créer un formulaire de connexion avec email et mot de passe, method post, action connexion.php
+sur connexion.php, vérifier que l'email et le mot de passe correspondent bien à une entrée dans la table users
+si c'est le cas, créer des variables de session :pseudo et email
+sinon message d'erreur: identifiants invalides
+-->
+    <header>Ouais</header>
+    
+</body>
+</html>
