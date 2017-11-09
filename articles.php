ed3bf81f0bd00ce68faa4386697b26edecf7f6ad
unknown <louis.desserteaux@gmail.com>
2017-11-09 09:41:56 +0100

    structure html : un message explicite

---
+++ articles.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>Ouais</header>
+        <nav class="navbar navbar-nav">
+            <li>
+                <ul><a href="index.php">Home</a></ul>
+                <ul><a href="articles.php">liste des articles</a></ul>
+                <ul><a href="ajouter_article.php">Ajouter un article</a></ul>
+                <ul><a href="modifier_article.php">Modifier un article</a></ul>
+                <ul><a href="supprimer_article.php">Supprimer un article</a></ul>
+            </li>
+        </nav>
+       
+    </body>
+</html>
